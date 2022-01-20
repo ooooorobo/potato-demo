665d28c6b28159ace4e55186b9294fa8a33ca229
ooooorobo <orobos654@gmail.com>
2022-01-20 21:52:51 +0900

    add post page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles/Main.css";
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
+import Post from "./pages/Post";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <h1>🥔 감자마켓</h1>
       </header>
       <Routes>
+        <Route path="/post/*" element={<Post />} />
         <Route path="/" element={<Main />} />
       </Routes>
     </div>
